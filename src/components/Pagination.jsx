994a3aa0beeecbb03389e30227d8526af2300dd4
Ashley Hsiao <ashley.hsiao@aboutyou.com>
2022-10-25 15:19:31 +0200

    Add abort and boolean flags

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,5 +1,11 @@
-const Pagination = props => {
-  return null;
+const Pagination = ({ page, onNext, onPrev }) => {
+  return (
+    <div className="flex item-center justify-center gap-3">
+      <button onClick={onPrev}>Prev</button>
+      <span>{page}</span>
+      <button onClick={onNext}>Next</button>
+    </div>
+  );
 };
 
 export default Pagination;
--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -2,30 +2,85 @@ import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import Pagination from "./Pagination";
 
-const Quotes = props => {
+const Quotes = (props) => {
   const [quotes, setQuotes] = useState([]);
+  const [page, setPage] = useState(1);
+  const abortRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  const initFetchQuotes = async () => {
+  const initFetchQuotes = async (page) => {
     try {
+      setIsError(false);
+      setIsLoading(true);
+      // abortRef.current?.(); // abort previous if there is one defined
+      // if (typeof abortRef.current === 'function') abortRef.current();
+
+      abortRef.current?.abort();
+
+      const controller = new AbortController();
+      // could just assign the abort and call per Line 12-13
+      // abortRef.current = controller.abort.bind(controller);
+
+      // could assign the whole controller then call per Line 15
+      abortRef.current = controller;
+      // controller.signal
+      // controller.abort
       const quotesData = await axios.get(
-        `http://localhost:4000/quotes?_page=1}`
+        `http://localhost:4000/quotes?_page=${page}}`,
+        {
+          signal: controller.signal,
+        }
+        // `http://localhost:4000/quotes`,
+        // {
+        //   params: {
+        //     _page: page,
+        //   },
+        // }
       );
       setQuotes(quotesData.data);
     } catch (error) {
       console.error(error);
+      if (error.name === "CanceledError") {
+        console.warn(`Previous request for ${page} was cancelled`);
+      } else {
+        setIsError(true);
+      }
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  const onPrev = () => {
+    if (page === 1) return;
+    setPage((page) => page - 1);
+  };
+
+  const onNext = () => {
+    // TODO: Check what is the last page, as can't keep going further
+    setPage((page) => page + 1);
+  };
+
   useEffect(() => {
-    initFetchQuotes();
-  }, []);
+    initFetchQuotes(page);
+  }, [page]);
 
   return (
     <div className="max-w-xl mx-auto">
       <h2 className="font-semibold text-2xl mb-4">Quotes</h2>
 
       <div>
-        {quotes.map(quote => {
+        <Pagination page={page} onPrev={onPrev} onNext={onNext} />
+
+        {isLoading ? <p>Loading quotes...</p> : null}
+        {isError ? (
+          <div>
+            <p>Something went wrong...</p>
+            <button onClick={() => initFetchQuotes(page)}>Retry</button>
+          </div>
+        ) : null}
+
+        {quotes.map((quote) => {
           return (
             <blockquote
               key={quote.id}
